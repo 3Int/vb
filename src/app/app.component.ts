d2090f30d4e92d9dc4fc4a76c9449d542d401ca0
eneller <erikneller@gmx.de>
2024-11-27 23:40:34 +0100

    basic ui alignment, add nTeams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButtonToggleModule} from '@angular/material/button-toggle'; 
+import {MatButtonToggleChange, MatButtonToggleModule} from '@angular/material/button-toggle'; 
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonToggleModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatListModule],
+  imports: [RouterOutlet, MatButtonToggleModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatListModule, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'vb';
+  numTeamsSelected = "2";
+  onNumTeamsSelector(event: MatButtonToggleChange): void{
+    this.numTeamsSelected = event.value;
+  }
 }
