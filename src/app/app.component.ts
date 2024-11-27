97f01f924a0a849386b75c76f7a404352ec127f8
eneller <erikneller@gmx.de>
2024-11-27 22:37:29 +0100

    add basic ui elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonToggleModule} from '@angular/material/button-toggle'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonToggleModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
