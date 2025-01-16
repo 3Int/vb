83bfe51282794a5787b9634f29ad9d26377b015d
eneller <erikneller@gmx.de>
2025-01-16 07:55:05 +0100

    Squashed commit of the following:
    commit 6596d30bd6a2f568b25c1938b1c7485e0ad4bd94
    Author: eneller <erikneller@gmx.de>
    Date:   Mon Jan 6 18:32:41 2025 +0100
    
        center nTeamsSelector
    
    commit 497b64fe3f32bcbcc95acc9f4d46ecd6c52d8947
    Author: eneller <erikneller@gmx.de>
    Date:   Mon Jan 6 15:25:24 2025 +0100
    
        remove angular material completely
    
    commit 4d5888049317ccaac792c09560231ddb3cef5d0f
    Author: eneller <erikneller@gmx.de>
    Date:   Mon Jan 6 15:23:05 2025 +0100
    
        replace angular material components with bootstrap
    
    commit dcf25ea9696f2c437d870edb5f402a5ff425efe0
    Author: eneller <erikneller@gmx.de>
    Date:   Mon Jan 6 13:13:05 2025 +0100
    
        add ng-bootstrap
    
    commit 48bbdab30894fff7606bd4349318d6247d9e5e2f
    Author: eneller <erikneller@gmx.de>
    Date:   Thu Nov 28 11:07:07 2024 +0100
    
        implement nTeams
    
    commit f7f8482bc91c2e003dc3342f9100932762ae4037
    Author: eneller <erikneller@gmx.de>
    Date:   Thu Nov 28 09:12:38 2024 +0100
    
        update icons
    
    commit e0a319b32a4b9adf8981b3c755ed371cac2e55f8
    Author: eneller <erikneller@gmx.de>
    Date:   Thu Nov 28 08:48:14 2024 +0100
    
        make pwa for offline usage
    
    commit 4be3649d4349247904bcef528f1d2344e5aad238
    Author: eneller <erikneller@gmx.de>
    Date:   Thu Nov 28 00:53:27 2024 +0100
    
        add padding to nTeamsToggle
    
    commit 2a596b15c8db346ffba52cb66fba6cee1df97ec4
    Author: eneller <erikneller@gmx.de>
    Date:   Thu Nov 28 00:32:41 2024 +0100
    
        fully functional using angular
    
    commit d2090f30d4e92d9dc4fc4a76c9449d542d401ca0
    Author: eneller <erikneller@gmx.de>
    Date:   Wed Nov 27 23:40:34 2024 +0100
    
        basic ui alignment, add nTeams
    
    commit 97f01f924a0a849386b75c76f7a404352ec127f8
    Author: eneller <erikneller@gmx.de>
    Date:   Wed Nov 27 22:37:29 2024 +0100
    
        add basic ui elements
    
    commit 102f589869952b1dd0486ee011cf6ca5748b5715
    Author: eneller <erikneller@gmx.de>
    Date:   Wed Nov 27 22:01:32 2024 +0100
    
        add angular material
    
    commit 7f5978b226590da1efa8ebfaa018a96d7fbf49cd
    Author: eneller <erikneller@gmx.de>
    Date:   Wed Nov 27 20:15:11 2024 +0100
    
        Initial Angular Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [NgbModule, RouterOutlet, CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less'
+})
+export class AppComponent {
+  title = 'vb';
+  numTeamsSelectorValue = "2";
+  numTeamsSelected = 2;
+  nTeamsValue = "4";
+  teamsArray: string[][] = [];
+  displayedColumns = ["teamCount", "teamNames"];
+
+  onButtonGenerate(textinput: string): void{
+    if(this.numTeamsSelectorValue === 'n'){
+      this.numTeamsSelected  = Number(this.nTeamsValue);
+    }
+    else{
+      this.numTeamsSelected = Number(this.numTeamsSelectorValue);
+    }
+    let names = textinput
+        .split('\n')
+        .map(function(str){return str.trim();})
+        .filter(function(str){return str}); // boolean interpretation is same as non-empty
+    // remove duplicates by using a Set
+    names = [...new Set(names)];
+    
+    var teams = Array.from({ length: this.numTeamsSelected }, () => []);
+    var playersPerTeam = Math.floor(names.length / this.numTeamsSelected);
+
+    let nameslen = names.length;
+    function* iter(list: any){
+        let index = 0;
+        while(true){
+            yield list[index % list.length];
+            index++;
+        }
+    }
+    var iterator = iter(teams);
+    for(let i =0; i < nameslen; i++){
+        var index = Math.floor(Math.random()* names.length);
+        var n = names[index]; 
+        names.splice(index,1);
+        var team = iterator.next().value;
+        team.push(n);
+
+    }
+    this.teamsArray = teams;
+  }
+
+}
