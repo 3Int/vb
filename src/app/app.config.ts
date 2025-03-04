e24f9794c5ab47fb9e7cd9d3dac23930595b04b4
eneller <erikneller@gmx.de>
2025-03-04 23:07:45 +0100

    fix: serviceworker doesnt intercept other urls

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,8 +7,10 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync('noop'), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+              provideClientHydration(withEventReplay()), provideAnimationsAsync('noop'),
+              provideServiceWorker('ngsw-worker.js', {
+                enabled: !isDevMode(),
+                registrationStrategy: 'registerWhenStable:30000'
           })]
 };
